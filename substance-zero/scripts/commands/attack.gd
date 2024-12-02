471c26480a4d8227ccbbcba29251bed851987d4b
ruohan8 <146478920+ruohan8@users.noreply.github.com>
2024-12-02 15:46:08 -0800

    Revert "Merge branch 'game-logic' into animation_tree"
    This reverts commit e59c60fc5e766e58e0c7693f06c1a1479842f01f, reversing
    changes made to 5efceedea37285cafbfc6b623b3625f12ebe5af4.

--- substance-zero/scripts/commands/attack.gd
+++
@@ -1,8 +0,0 @@
-# for now, from exercise 1
-class_name AttackCommand
-extends Command
-
-
-func execute(_character:Character) -> Status:
-	print('attacked')
-	return Status.DONE
--- substance-zero/scripts/commands/idle_command.gd
+++
@@ -1,9 +0,0 @@
-# modified from exercise 1
-class_name IdleCommand
-extends Command
-
-
-func execute(character:Character) -> Status:
-	character.velocity.x = 0
-	character.velocity.y = 0
-	return Status.DONE
