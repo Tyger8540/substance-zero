4ac0c15e1b8300d82c643edfdc46cde1f62da2f2
ChKeighley <123129785+ChKeighley@users.noreply.github.com>
2024-12-03 17:13:17 -0800

    Moved world.tscn out of folder

--- substance-zero/scripts/UI/start_menu.gd
+++ substance-zero/scripts/UI/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func _on_options_button_pressed() -> void:
