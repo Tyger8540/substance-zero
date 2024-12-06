9c1fc65b35cc44fb8b2910c5c22a0e72d8f1ae2a
Ambott967 <ambott@ucdavis.edu>
2024-12-06 14:11:47 -0800

    Polished ship movement feel

--- substance-zero/scripts/commands/ship_movement/deccelerate_command.gd
+++ substance-zero/scripts/commands/ship_movement/deccelerate_command.gd
@@ -3,15 +3,9 @@ extends Command
 
 # TODO need to make work
 func execute(character:Character) -> Status:
-	#var ship_velocity:Vector2 = Vector2(0.0, 1.0)
-	#ship_velocity = ship_velocity.rotated(character.rotation) * character.ship_decceleration
-	#character.velocity += ship_velocity
-	#character.velocity = clampf(character.velocity.length(), 0.0, 100)
-	
-	#var ship_velocity:Vector2 = Vector2(0.0, -1.0)
-	#ship_velocity = ship_velocity.rotated(character.rotation) * character.ship_decceleration
-	#character.velocity -= ship_velocity
-	
-	character.velocity.move_toward(character.position, 1)
+	if character.velocity.length() > 0:
+		character.velocity /= character.ship_decceleration
+	if character.velocity.length() < 12:
+		character.velocity = Vector2(0.0, 0.0)
 	
 	return Status.DONE
