c7860e8807568a2d3ff8bd84330b74959aba71a3
Tyger8540 <tymatson2015@gmail.com>
2024-12-08 13:28:55 -0800

    Added Functionality to Add or Remove Powerups from Inventory

--- substance-zero/scripts/autoload/power_up_inventory.gd
+++ substance-zero/scripts/autoload/power_up_inventory.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var power_ups: Array[PowerUp]
+
 # Store Power up Levels
 # Different Powerups Can Have Different Lifespans, i.e. both a Permanent Melee Damage and Limited Melee Damage Upgrade
 var power_up_level = {
@@ -16,3 +18,13 @@ var power_up_level = {
 		Enums.Power_Up_Type.GUN_DAMAGE: 0,
 	},
 }
+
+
+func add_power_up(power_up: PowerUp) -> void:
+	power_up_level[Enums.Power_Up_Lifespan][Enums.Power_Up_Type] += 1
+	power_ups.append(power_up)
+
+
+func remove_power_up(power_up: PowerUp) -> void:
+	power_up_level[Enums.Power_Up_Lifespan][Enums.Power_Up_Type] -= 1
+	power_ups.erase(power_up)
