f6ae89b3aa42108b559454f63df1a0bfbcef94f0
Ambott967 <ambott@ucdavis.edu>
2024-12-09 19:58:53 -0800

    Implemented space autoscroll camera

--- substance-zero/scripts/commands/ship_movement/deccelerate_command.gd
+++ substance-zero/scripts/commands/ship_movement/deccelerate_command.gd
@@ -5,6 +5,6 @@ extends Command
 func execute(character:Character) -> Status:
 	if character.velocity.length() > 0:
 		character.velocity /= character.ship_decceleration
-	if character.velocity.length() < 12:
-		character.velocity = Vector2(0.0, 0.0)
+	#if character.velocity.length() < 12:
+		#character.velocity = Vector2(0.0, 0.0)
 	return Status.DONE
