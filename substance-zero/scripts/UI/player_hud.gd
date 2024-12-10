92bb8768c3195c6f36af8d8fb9995ae4a22bd7fe
ChKeighley <123129785+ChKeighley@users.noreply.github.com>
2024-12-10 01:46:50 -0800

    HUD started
    - Adding visual elements for player's HUD

---
+++ substance-zero/scripts/UI/player_hud.gd
@@ -0,0 +1,14 @@
+class_name HUD
+extends Control
+
+
+@onready player: Player = get_node("../../Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
