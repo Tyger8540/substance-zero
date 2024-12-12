7bbf80ae76860c9e671dbbd95dd14ee5b6fa045b
Ambott967 <ambott@ucdavis.edu>
2024-12-11 23:38:16 -0800

    Some space scene implementation

--- substance-zero/scripts/cameras/space_autoscroll_camera.gd
+++ substance-zero/scripts/cameras/space_autoscroll_camera.gd
@@ -11,23 +11,25 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	global_position.x += scroll_speed
+	#global_position.x += scroll_speed
+	#
+	#var trail_distance = global_position.x - subject.global_position.x
+	#if trail_distance > max_lead:
+		#if subject.velocity.x < 0:
+			#subject.velocity.x = 0
+		#subject.global_position.x += trail_distance - max_lead
+	#
+	#var lead_distance = subject.global_position.x - global_position.x
+	#if lead_distance > max_lead:
+		#if subject.velocity.x > 0:
+			#subject.velocity.x = 0
+		#subject.global_position.x -= lead_distance - max_lead
+	#
+	#var verticle_distance = global_position.y - subject.global_position.y
+	#if abs(verticle_distance) > _verticle_boundary_distance:
+		#if verticle_distance > 0:
+			#subject.global_position.y += 648
+		#else:
+			#subject.global_position.y -= 648
 	
-	var trail_distance = global_position.x - subject.global_position.x
-	if trail_distance > max_lead:
-		if subject.velocity.x < 0:
-			subject.velocity.x = 0
-		subject.global_position.x += trail_distance - max_lead
-	
-	var lead_distance = subject.global_position.x - global_position.x
-	if lead_distance > max_lead:
-		if subject.velocity.x > 0:
-			subject.velocity.x = 0
-		subject.global_position.x -= lead_distance - max_lead
-	
-	var verticle_distance = global_position.y - subject.global_position.y
-	if abs(verticle_distance) > _verticle_boundary_distance:
-		if verticle_distance > 0:
-			subject.global_position.y += 648
-		else:
-			subject.global_position.y -= 648
+	global_position = subject.global_position
