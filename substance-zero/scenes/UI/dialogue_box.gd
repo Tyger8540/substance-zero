9cfc8875ea97306cfda7fb19fea2e7a978c28471
ruohan8 <146478920+ruohan8@users.noreply.github.com>
2024-12-12 16:02:12 -0800

    fix cutscene scripts

--- substance-zero/scenes/UI/dialogue_box.gd
+++ substance-zero/scenes/UI/dialogue_box.gd
@@ -3,3 +3,4 @@ extends Control
 
 
 @onready var dialogue: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/MarginContainer/Dialogue
+@onready var character_name: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/CharacterName
